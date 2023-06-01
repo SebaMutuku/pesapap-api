45e8ad989eb22588234020cd09f5681a36511d20
SebaMutuku <sebastian.muthiani@gmail.com>
2023-06-01 10:13:56 +0300

    Add exception when creating a student

--- src/main/java/com/pesapap/apiv1/services/StudentService.java
+++ src/main/java/com/pesapap/apiv1/services/StudentService.java
@@ -23,8 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class StudentService extends StudentServiceImpl {
     private final StudentRepo studentRepo;
 
-    @Autowired
-    public StudentService(StudentRepo studentRepo) {
+    public StudentService(@Autowired StudentRepo studentRepo) {
         this.studentRepo = studentRepo;
 
     }
@@ -32,48 +31,54 @@ public class StudentService extends StudentServiceImpl {
     @Override
     @Transactional
     public StudentPaymentResponse createStudent(Student student) {
-        log.info("Request  [{}]", student);
-        Optional<Student> dbStudent = studentRepo.findByRegistrationId(student.getRegistrationId());
-        return dbStudent.map(existingStudent ->
-                        new StudentPaymentResponse(existingStudent, "Student already exists", HttpStatus.ALREADY_REPORTED))
-                .orElseGet(() -> {
-                    StudentPaymentResponse studentPaymentResponse;
-                    if (student.getRegistrationId() != null && student.getFullName() != null && student.getCourseName() != null) {
-                        Student sessionStudent = Student.builder()
-                                .courseName(student.getCourseName())
-                                .fullName(student.getFullName())
-                                .feeBalance(0.0)
-                                .annualFee(87000.00)
-                                .paidFees(0.00)
-                                .paymentChannel(null)
-                                .registrationId(student.getRegistrationId())
-                                .build();
-                        studentRepo.save(sessionStudent);
-                        studentPaymentResponse = new StudentPaymentResponse(sessionStudent, "Success", HttpStatus.CREATED);
-                        log.info("Response [{}]", studentPaymentResponse);
+        log.info("Fee payment request ----> [{}]", student);
+        try {
+            Optional<Student> dbStudent = studentRepo.findByRegistrationId(student.getRegistrationId());
+            return dbStudent.map(existingStudent ->
+                            new StudentPaymentResponse(existingStudent, "Student already exists", HttpStatus.ALREADY_REPORTED))
+                    .orElseGet(() -> {
+                        StudentPaymentResponse studentPaymentResponse;
+                        if (student.getRegistrationId() != null && student.getFullName() != null && student.getCourseName() != null) {
+                            Student sessionStudent = Student.builder()
+                                    .courseName(student.getCourseName())
+                                    .fullName(student.getFullName())
+                                    .feeBalance(0.0)
+                                    .annualFee(87000.00)
+                                    .paidFees(0.00)
+                                    .paymentChannel(null)
+                                    .registrationId(student.getRegistrationId())
+                                    .build();
+                            studentRepo.save(sessionStudent);
+                            studentPaymentResponse = new StudentPaymentResponse(sessionStudent, "Success", HttpStatus.CREATED);
+                            log.info("Payment response ---> [{}]", studentPaymentResponse);
+                            return studentPaymentResponse;
+                        }
+                        studentPaymentResponse = new StudentPaymentResponse(null, "Invalid Student data", HttpStatus.BAD_REQUEST);
+                        log.error("Payment response ---> [{}]", studentPaymentResponse);
                         return studentPaymentResponse;
-                    }
-                    studentPaymentResponse = new StudentPaymentResponse(null, "Invalid Student data", HttpStatus.BAD_REQUEST);
-                    log.info("Response [{}]", studentPaymentResponse);
-                    return studentPaymentResponse;
-                });
+                    });
+        } catch (Exception exception) {
+            StudentPaymentResponse studentPaymentResponse = new StudentPaymentResponse(null, exception.getMessage(), HttpStatus.EXPECTATION_FAILED);
+            log.error("Exception ---> ", exception);
+            return studentPaymentResponse;
+        }
     }
 
     @Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
     @Override
     public StudentValidationResponse findById(String registrationId) {
         try {
-            log.info("Registration id [{}]", registrationId);
+            log.info("Registration id ---> [{}]", registrationId);
             Optional<StudentValidationResponse> studentValidationResponse = studentRepo.findByRegistrationId(registrationId)
                     .map(student -> new StudentValidationResponse(student, "Success", HttpStatus.OK));
-            studentValidationResponse.ifPresent(response -> log.info("Student [{}]", response));
+            studentValidationResponse.ifPresent(response -> log.info("Validation response ----> [{}]", response));
             return studentValidationResponse.orElse(new StudentValidationResponse(null, "Student with that registration not found", HttpStatus.NOT_FOUND));
         } catch (Exception e) {
             StudentValidationResponse studentValidationResponse = new StudentValidationResponse(
                     null,
                     e.getMessage(),
                     HttpStatus.NOT_FOUND);
-            log.error("Response object ", e);
+            log.error("Exception ----> ", e);
             return studentValidationResponse;
         }
     }
@@ -82,7 +87,7 @@ public class StudentService extends StudentServiceImpl {
     @Override
     public StudentPaymentResponse paymentResponse(PaymentRequest paymentRequest) {
         try {
-            log.info("Request [{}]", paymentRequest);
+            log.info("Fee payment request ---> [{}]", paymentRequest);
             StudentDTO paymentInfo = paymentRequest.payload();
             Optional<StudentPaymentResponse> studentPaymentResponse = studentRepo.findByRegistrationId(paymentRequest.payload().getRegNumber())
                     .map(student -> {
@@ -92,11 +97,11 @@ public class StudentService extends StudentServiceImpl {
                         Student savedStudent = studentRepo.save(student);
                         return new StudentPaymentResponse(savedStudent, "Success", HttpStatus.OK);
                     });
-            studentPaymentResponse.ifPresent(response -> log.error("Student object:: [{}]", response));
+            studentPaymentResponse.ifPresent(response -> log.info("Response body when creating a student ----> [{}]", response));
             return studentPaymentResponse.orElse(new StudentPaymentResponse(null, "An Error occurred", HttpStatus.EXPECTATION_FAILED));
         } catch (Exception exception) {
             StudentPaymentResponse studentPaymentResponse = new StudentPaymentResponse(null, exception.getMessage(), HttpStatus.EXPECTATION_FAILED);
-            log.error("Response  object ---> ", exception);
+            log.error("Exception ---> ", exception);
             return studentPaymentResponse;
         }
     }
